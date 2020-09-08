2daf0dd6b0e88e17ac558e6f088a958e04c24ec1
konerub <39737028+konerub@users.noreply.github.com>
2020-09-08 09:07:16 -0400

    Update create-ec2.tf

--- create-ec2.tf
+++ create-ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "bkEC2" {
-  ami                                  = data.aws_ami.myAMI.id
+  ami                                  = "ami-02354e95b39ca8dec"
   instance_type                        = "t2.micro"
   key_name                             = "bk"
   instance_initiated_shutdown_behavior = "stop"
